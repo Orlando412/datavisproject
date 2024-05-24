0cdfc31461aed841bff7550b83671f3a01b07570
Orlando Adams <ojadams@student.fullsail.edu>
2024-05-24 19:32:40 -0400

    set up the pipeline options

--- main.py
+++ main.py
@@ -3,3 +3,14 @@ from apache_beam.options.pipeline_options import PipelineOptions, GoogleCloudOpt
 
 
 #sets up the pipeline options 
+option = PipelineOptions()
+gcp_options = option.view_as(GoogleCloudOptions)
+gcp_options.project = 'datavisprojects'
+gcp_options.region = 'us-central1'
+gcp_options.job_name = 'pubsub-to-bigquery'
+gcp_options.staging_location = 'gs://buchet_number_1/staging'
+gcp_options.temp_location = 'gs://buchet_number_1/temp'
+option.view_as(PipelineOptions).streaming = True
+
+
+
